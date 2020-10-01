c51ca44c76d28472cd16c07fddbb91f2ce692f87
Veena Uppalapati <vuveenauppalapati@gmail.com>
2020-09-30 21:43:53 -0400

    PyPoll: Total number of Vote casts

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,9 @@
+
+# Import required modules
 import os
 import csv
 
+# Define read_input_file function:  Read the election_data.csv file and push the data into a list
 def read_input_file(input_list):
 
     # define the path for the input file
@@ -13,22 +16,31 @@ def read_input_file(input_list):
 
         # Read the header row first (skip this step if there is now header)
         csv_header = next(csv_reader)
-
-
-        
+    
 
         #Read each each row of data after the header
         for row in csv_reader:
 
             input_list.append([int(row[0]), row[1], row[2]])
 
-          
+# --------------------------------------------------------------------------------
+
+# Define the main function       
 
 def main():
 
     election_data_list = []
+    total_votes = 0
+    
+    # Call read_input_file function: Read the election_data.csv file and push the data into a list
     read_input_file(election_data_list)
+ 
+    #calculate the total number of votes cast
+    total_votes = len(election_data_list)
 
-    print(election_data_list[0])
 
+    # Display the output
+    print(f"Total Votes: {total_votes}")
+
+# --------------------------------------------------------------------------------
 main()
